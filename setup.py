c054f03a57abc5d8412809d9fd0bf90535756fee
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2017-01-22 12:13:16 -0800

    added hocomoco to package data

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ config = {
     'download_url': 'https://github.com/kundajelab/simdna',
     'version': '0.3',
     'packages': ['simdna', 'simdna.resources'],
-    'package_data': {'simdna.resources': ['encode_motifs.txt.gz']},
+    'package_data': {'simdna.resources': ['encode_motifs.txt.gz', 'HOCOMOCOv10_HUMAN_mono_homer_format_0.001.motif.gz']},
     'setup_requires': [],
     'install_requires': ['numpy>=1.9', 'matplotlib', 'scipy'],
     'dependency_links': [],
