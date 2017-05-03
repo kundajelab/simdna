e53fb93a2fbb8b47c0e5aae43ac66cd5eb7c6242
jisraeli <jisraeli@stanford.edu>
2017-05-03 02:47:47 -0700

    seeding the random number generator

--- simdna/__init__.py
+++ simdna/__init__.py
@@ -3,3 +3,4 @@ from pkg_resources import resource_filename
 ENCODE_MOTIFS_PATH = resource_filename('simdna.resources', 'encode_motifs.txt.gz')
 HOCOMOCO_MOTIFS_PATH = resource_filename('simdna.resources', 'HOCOMOCOv10_HUMAN_mono_homer_format_0.001.motif.gz')
 random = random.Random()
+random.seed(1)
